862e3f484870108a1c02a654e2fa36966ee856ee
udcxx <me@udcxx.me>
2022-07-22 23:03:40 +0900

    アプリ側で読み込むJSとCSSをアップ

---
+++ cdn/print_button.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+    kintone.events.on('app.record.detail.show', function(event) {
+      const header = kintone.app.record.getHeaderMenuSpaceElement();
+      const button = new Kuc.Button({
+        text: '印刷 by kinxper',
+        type: 'submit',
+        className: 'options-class',
+        id: 'kinxper-print-button',
+        visible: true,
+        disabled: false
+      });
+      header.appendChild(button);
+      
+      const recordId = kintone.app.record.getId();
+      const appId = kintone.app.getId();
+      const token = event.record['Token'].value;
+      const domain = location.hostname.replace('.cybozu.com', '');
+  
+      button.addEventListener('click', function(event) {
+        window.open(`https://app.udcxx.me/kinxper/?domain=${domain}&appid=${appId}&recordid=${recordId}&token=${token}`, '_blank', 'noreferrer');
+      });
+      
+      return event;
+    });
+  })();
